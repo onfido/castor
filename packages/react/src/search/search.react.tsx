2ae353bfa4301bf196ebd54bcda5620e99adaad9
Andre Rabello <andre.rabello@onfido.com>
2020-12-17 19:16:19 +0000

    docs: search component

--- packages/react/src/search/search.react.tsx
+++ packages/react/src/search/search.react.tsx
@@ -4,6 +4,12 @@ import React from 'react';
 import { Input, InputProps } from '../input/input.react';
 import { withRef } from '../utils';
 
+/**
+ * @note `Search` uses an `Icon` that requires
+ * `Icons` (SVG sprite) to be included in your app.
+ *
+ * https://github.com/onfido/castor-icons#use-with-plain-code
+ */
 export const Search = withRef(
   ({ className, style, ...props }: SearchProps, ref: SearchProps['ref']) => (
     <div className={classy(c('search'), className)} style={style}>
--- packages/react/src/utils/withRef.ts
+++ packages/react/src/utils/withRef.ts
@@ -2,7 +2,7 @@ import { FC, forwardRef, ForwardRefRenderFunction } from 'react';
 
 /**
  * Same as `forwardRef` except it returns the same type of the `component` going in.
- * @param component Component to forwardRef.
+ * @param component Component to `forwardRef`.
  */
 export const withRef = <Props>(component: FC<Props>) =>
   forwardRef(component as ForwardRefRenderFunction<Props>) as typeof component;
