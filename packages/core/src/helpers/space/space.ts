8503e044ddc3b1b0351c6f61a61c0e8f2d01cebb
Andre Rabello <andre.rabello@onfido.com>
2020-12-18 12:51:25 +0000

    revert: note comment

--- packages/core/src/helpers/space/space.ts
+++ packages/core/src/helpers/space/space.ts
@@ -1,7 +1,7 @@
 /**
  * Returns the `px` size of a multiplier of the base space.
  *
- * @note Only integers and (1.5, 0.5, 0.25, 0.125) are supported.
+ * Only integers and (1.5, 0.5, 0.25, 0.125) are supported.
  *
  * @param multiplier How much to multiply the base.
  *
