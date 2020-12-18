77c47e2c82be0ec17bacc2fe24c1bbbf04330352
Andre Rabello <andre.rabello@onfido.com>
2020-12-18 16:24:19 +0000

    refactor: test

--- packages/core/src/helpers/space/space.spec.ts
+++ packages/core/src/helpers/space/space.spec.ts
@@ -2,22 +2,20 @@ import { describe, expect, it } from '@jest/globals';
 import { space } from './space';
 
 describe('space', () => {
-  const allowedFloats = [1.5, 0.5, 0.25, 0.125];
-  allowedFloats.toString = () => `(${allowedFloats.join(', ')})`;
-  const base = 8;
-
   it('should return the correct "multiplier * base" value', () => {
     expect(space(1)).toBe('8px');
   });
 
-  it(`should allow ${allowedFloats}`, () => {
-    for (const float of allowedFloats)
-      expect(space(float)).toBe(`${float * base}px`);
+  it('should allow (1.5, 0.5, 0.25, 0.125)', () => {
+    expect(space(1.5)).toBe('12px');
+    expect(space(0.5)).toBe('4px');
+    expect(space(0.25)).toBe('2px');
+    expect(space(0.125)).toBe('1px');
   });
 
-  it(`should throw for non integers and not in ${allowedFloats}`, () => {
-    expect(() => space(2.5)).toThrow(
-      `"multiplier" must be an integer or ${allowedFloats}`
+  it('should throw for non integers or not in (1.5, 0.5, 0.25, 0.125)', () => {
+    expect(() => space(2.5)).toThrowError(
+      '"multiplier" must be an integer or (1.5, 0.5, 0.25, 0.125)'
     );
   });
 });
