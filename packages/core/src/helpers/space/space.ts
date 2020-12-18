00ce6c63a24e15b55a0a76567eb12bb472da7bf4
Andre Rabello <andre.rabello@onfido.com>
2020-12-18 11:44:36 +0000

    refactor: space

--- packages/core/src/helpers/space/space.ts
+++ packages/core/src/helpers/space/space.ts
@@ -1,7 +1,7 @@
 /**
  * Returns the `px` size of a multiplier of the base space.
  *
- * Only integers and (1.5, 0.5, 0.25, 0.125) are supported.
+ * @note Only integers and (1.5, 0.5, 0.25, 0.125) are supported.
  *
  * @param multiplier How much to multiply the base.
  *
@@ -12,15 +12,16 @@
 export function space(multiplier: number): string {
   if (!isAllowed(multiplier))
     throw new Error(
-      `"multiplier" must be an integer or (${floats}). Got: ${multiplier}`
+      `"multiplier" must be an integer or (${allowedFloats}). Got: ${multiplier}`
     );
 
   return `${multiplier * base}px`;
 }
 
-const allowedFractionals = new Set([1.5, 0.5, 0.25, 0.125]);
+const allowedFloats = new Set([1.5, 0.5, 0.25, 0.125]);
+allowedFloats.toString = () => [...allowedFloats].join(', ');
+
 const base = 8;
-const floats = [...allowedFractionals].join(', ');
 
 const isAllowed = (multiplier: number) =>
-  Number.isInteger(multiplier) || allowedFractionals.has(multiplier);
+  Number.isInteger(multiplier) || allowedFloats.has(multiplier);
