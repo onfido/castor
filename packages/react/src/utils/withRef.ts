8b4d8b6928bd7053150b19a16815e28a4a63d8ef
Andre Rabello <andre.rabello@onfido.com>
2020-12-21 18:02:04 +0000

    style: change eslint ignore

--- packages/react/src/utils/withRef.ts
+++ packages/react/src/utils/withRef.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { FC, forwardRef } from 'react';
 
 /**
@@ -6,5 +5,7 @@ import { FC, forwardRef } from 'react';
  *
  * @param component Component to `forwardRef`.
  */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const withRef = <C extends FC<any>>(component: C): C =>
   forwardRef(component as any) as any;
+/* eslint-enable @typescript-eslint/no-explicit-any */
