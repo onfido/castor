faf88e118e40f9cf545571dc8bfb94366c353649
Julius Osokinas <20243687+josokinas@users.noreply.github.com>
2020-12-31 11:45:30 +0000

    fix(react): export search component (#141)

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,4 +1,5 @@
 export * from './button/button.react';
 export * from './icon/icon.react';
 export * from './input/input.react';
+export * from './search/search.react';
 export * from './textarea/textarea.react';
