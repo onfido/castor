5818a9172ae64fdfeed6645385a2a01c238e08bf
Julius Osokinas <julius.osokinas@onfido.com>
2021-01-08 11:46:21 +0200

    feat: allow control externally and internally

--- packages/react/src/components/radio/radio.react.tsx
+++ packages/react/src/components/radio/radio.react.tsx
@@ -1,39 +1,52 @@
 import { c, classy, RadioProps as BaseProps, m } from '@onfido/castor';
-import React from 'react';
+import React, { useState } from 'react';
 import { withRef } from '../../utils';
 
 export const Radio = withRef(
   (
     {
       bordered,
-      checked,
+      defaultChecked = false,
+      checked: externalChecked,
       invalid,
       disabled,
       children,
       className,
       style,
+      onChange,
       ...restProps
     }: RadioProps,
     ref: RadioProps['ref']
-  ): JSX.Element => (
-    <label
-      className={classy(
-        c('radio-label'),
-        m({ bordered, checked, disabled }),
-        className
-      )}
-      style={style}
-    >
-      <input
-        {...{ ...restProps, checked, disabled }}
-        ref={ref}
-        type="radio"
-        className={classy(c('radio-input'), m({ invalid }))}
-      />
-      <span /* custom "checked" indicator */ aria-hidden="true" />
-      {children && <span>{children}</span>}
-    </label>
-  )
+  ): JSX.Element => {
+    const [internalChecked, setInternalChecked] = useState(defaultChecked);
+
+    const externallyControlled = typeof externalChecked === 'boolean';
+    const checked = externallyControlled ? externalChecked : internalChecked;
+
+    return (
+      <label
+        className={classy(
+          c('radio-label'),
+          m({ bordered, checked, disabled }),
+          className
+        )}
+        style={style}
+      >
+        <input
+          {...{ ...restProps, checked, disabled }}
+          ref={ref}
+          type="radio"
+          className={classy(c('radio-input'), m({ invalid }))}
+          onChange={(e) => {
+            if (!externallyControlled) setInternalChecked(e.target.checked);
+            if (onChange) onChange(e);
+          }}
+        />
+        <span /* custom "checked" indicator */ aria-hidden="true" />
+        {children && <span>{children}</span>}
+      </label>
+    );
+  }
 );
 
 export type RadioProps = BaseProps &
