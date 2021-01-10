2adbbefeaf08c8505bd13bbd579593c753da0538
Julius Osokinas <julius.osokinas@onfido.com>
2021-01-10 11:05:11 +0200

    feat(core): allow space to be used with any number

--- packages/core/src/helpers/space/space.ts
+++ packages/core/src/helpers/space/space.ts
@@ -1,8 +1,6 @@
 /**
  * Returns the `px` size of a multiplier of the base space.
  *
- * Only integers and (2.5, 1.5, 0.5, 0.25, 0.125) are supported.
- *
  * @param multiplier How much to multiply the base.
  *
  * @example
@@ -10,16 +8,7 @@
  * // '32px'
  */
 export function space(multiplier: number): string {
-  if (!isAllowed(multiplier))
-    throw new Error(`"multiplier" must be an integer or (${allowedFloats})`);
-
   return `${multiplier * base}px`;
 }
 
-const allowedFloats = new Set([2.5, 1.5, 0.5, 0.25, 0.125]);
-allowedFloats.toString = () => [...allowedFloats].join(', ');
-
 const base = 8;
-
-const isAllowed = (multiplier: number) =>
-  Number.isInteger(multiplier) || allowedFloats.has(multiplier);
