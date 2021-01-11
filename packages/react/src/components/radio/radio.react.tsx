548d2e1c460c541575c14245e761e43afcb175f7
Andre Rabello <andre.rabello@onfido.com>
2021-01-11 16:46:20 +0000

    chore: proof of concept

--- packages/react/src/components/radio/radio.react.tsx
+++ packages/react/src/components/radio/radio.react.tsx
@@ -1,52 +1,26 @@
-import { c, classy, RadioProps as BaseProps, m } from '@onfido/castor';
-import React, { useState } from 'react';
+import { c, classy, m, RadioProps as BaseProps } from '@onfido/castor';
+import React from 'react';
 import { withRef } from '../../utils';
 
 export const Radio = withRef(
   (
-    {
-      bordered,
-      defaultChecked = false,
-      checked: externalChecked,
-      invalid,
-      disabled,
-      children,
-      className,
-      style,
-      onChange,
-      ...restProps
-    }: RadioProps,
+    { bordered, invalid, children, className, style, ...restProps }: RadioProps,
     ref: RadioProps['ref']
-  ): JSX.Element => {
-    const [internalChecked, setInternalChecked] = useState(defaultChecked);
-
-    const externallyControlled = typeof externalChecked === 'boolean';
-    const checked = externallyControlled ? externalChecked : internalChecked;
-
-    return (
-      <label
-        className={classy(
-          c('radio-label'),
-          m({ bordered, checked, disabled }),
-          className
-        )}
-        style={style}
-      >
-        <input
-          {...{ ...restProps, checked, disabled }}
-          ref={ref}
-          type="radio"
-          className={classy(c('radio-input'), m({ invalid }))}
-          onChange={(e) => {
-            if (!externallyControlled) setInternalChecked(e.target.checked);
-            if (onChange) onChange(e);
-          }}
-        />
-        <span /* custom "checked" indicator */ aria-hidden="true" />
-        {children && <span>{children}</span>}
-      </label>
-    );
-  }
+  ): JSX.Element => (
+    <label
+      className={classy(c('radio-label'), m({ bordered }), className)}
+      style={style}
+    >
+      <input
+        {...restProps}
+        ref={ref}
+        type="radio"
+        className={classy(c('radio-input'), m({ invalid }))}
+      />
+      <span className={classy(c('radio-indicator'))} aria-hidden="true" />
+      {children && <span>{children}</span>}
+    </label>
+  )
 );
 
 export type RadioProps = BaseProps &
