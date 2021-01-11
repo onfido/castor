8241cb2f8c70e59cd436bc56d5f658e9daea7f4f
Julius Osokinas <20243687+josokinas@users.noreply.github.com>
2021-01-11 10:45:39 +0000

    feat(core): allow space to be used with any number (#156)

--- packages/core/src/helpers/space/space.spec.ts
+++ packages/core/src/helpers/space/space.spec.ts
@@ -2,21 +2,19 @@ import { describe, expect, it } from '@jest/globals';
 import { space } from './space';
 
 describe('space', () => {
-  it('should return the correct "multiplier * base" value', () => {
-    expect(space(1)).toBe('8px');
-  });
-
-  it('should allow (2.5, 1.5, 0.5, 0.25, 0.125)', () => {
-    expect(space(2.5)).toBe('20px');
-    expect(space(1.5)).toBe('12px');
-    expect(space(0.5)).toBe('4px');
+  it('should return the correct positive "multiplier * base" value', () => {
     expect(space(0.25)).toBe('2px');
-    expect(space(0.125)).toBe('1px');
+    expect(space(0.5)).toBe('4px');
+    expect(space(1)).toBe('8px');
+    expect(space(1.5)).toBe('12px');
+    expect(space(2.5)).toBe('20px');
   });
 
-  it('should throw for non integers or not in (2.5, 1.5, 0.5, 0.25, 0.125)', () => {
-    expect(() => space(0.75)).toThrowError(
-      '"multiplier" must be an integer or (2.5, 1.5, 0.5, 0.25, 0.125)'
-    );
+  it('should return the correct negative "multiplier * base" value', () => {
+    expect(space(-0.25)).toBe('-2px');
+    expect(space(-0.5)).toBe('-4px');
+    expect(space(-1)).toBe('-8px');
+    expect(space(-1.5)).toBe('-12px');
+    expect(space(-2.5)).toBe('-20px');
   });
 });
