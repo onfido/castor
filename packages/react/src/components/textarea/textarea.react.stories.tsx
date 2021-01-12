6b1a9d19d695d61587ca4735b71ea8f5bec13841
Julius Osokinas <20243687+josokinas@users.noreply.github.com>
2021-01-12 10:33:09 +0000

    docs: better describe textarea component "resize" variations (#176)

--- packages/react/src/components/textarea/textarea.react.stories.tsx
+++ packages/react/src/components/textarea/textarea.react.stories.tsx
@@ -2,6 +2,14 @@ import { Textarea, TextareaProps } from '@onfido/castor-react';
 import React from 'react';
 import { Meta, omit, Story, storyOf } from '../../../../../docs';
 
+const resizes: readonly TextareaProps['resize'][] = [
+  'vertical',
+  'horizontal',
+  'both',
+  'none',
+];
+resizes.toString = () => resizes.map((value) => `"${value}"`).join('|');
+
 export default {
   title: 'React/Textarea',
   component: Textarea,
@@ -10,12 +18,14 @@ export default {
     placeholder: { control: 'text' },
     resize: {
       table: {
-        type: { summary: 'string' },
+        type: {
+          summary: resizes.toString(),
+        },
         defaultValue: { summary: 'vertical' },
       },
       control: {
-        type: 'select',
-        options: ['vertical', 'horizontal', 'both', 'none'],
+        type: 'radio',
+        options: resizes,
       },
     },
     rows: {
@@ -45,15 +55,10 @@ export const Playground: Story<TextareaProps> = (props: TextareaProps) => (
   <Textarea {...props} />
 );
 
-export const Resize = storyOf(
-  Textarea,
-  'resize',
-  ['vertical', 'horizontal', 'both', 'none'],
-  {
-    labelMode: 'prop',
-    labelProp: 'defaultValue',
-  }
-);
+export const Resize = storyOf(Textarea, 'resize', resizes, {
+  labelMode: 'prop',
+  labelProp: 'defaultValue',
+});
 Resize.argTypes = omit<TextareaProps>('resize');
 
 export const Invalid = storyOf(Textarea, 'invalid', [true, false], {
