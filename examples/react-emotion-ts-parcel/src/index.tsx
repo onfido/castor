cf09bca41b958fe98f3db061274cf87ef6d563d3
Andre Rabello <andre.rabello@onfido.com>
2021-01-19 18:20:34 +0000

    build: automatically sort imports

--- examples/react-emotion-ts-parcel/src/index.tsx
+++ examples/react-emotion-ts-parcel/src/index.tsx
@@ -1,7 +1,7 @@
+import '@onfido/castor/dist/castor.css';
+import '@onfido/castor/dist/themes/day.css';
 import React from 'react';
 import { render } from 'react-dom';
 import { App } from './App';
-import '@onfido/castor/dist/castor.css';
-import '@onfido/castor/dist/themes/day.css';
 
 render(<App />, document.getElementById('root'));
