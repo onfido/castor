59550b25d1fea6ca112cf7d2de6c83a49cecfacb
Andre Rabello <andre.rabello@onfido.com>
2021-01-28 12:48:44 +0000

    refactor: const to function

--- packages/react/src/internal/indicator-container/splitContainerProps.ts
+++ packages/react/src/internal/indicator-container/splitContainerProps.ts
@@ -3,12 +3,12 @@ import { partition } from '../../utils';
 /**
  * Splits `Indicator` properties into `label` and `input` props.
  */
-export const splitContainerProps = <T extends Input | Label>(props: T) => {
+export function splitContainerProps<T extends Input | Label>(props: T) {
   const entries = Object.entries(props);
   const splitProps = partition(entries, ([key]) => containerProps.test(key));
 
   return splitProps.map(Object.fromEntries) as [Label, Input];
-};
+}
 
 const containerProps = /^aria-|^data-/;
 
