d754f4f08d69633b07876cc57a0642cba1431a36
Andre Rabello <andre.rabello@onfido.com>
2021-01-28 10:36:12 +0000

    chore: rename variable

--- packages/react/src/internal/indicator-container/splitContainerProps.ts
+++ packages/react/src/internal/indicator-container/splitContainerProps.ts
@@ -5,12 +5,12 @@ import { partition } from '../../utils';
  */
 export const splitContainerProps = <T extends Input | Label>(props: T) => {
   const entries = Object.entries(props);
-  const splitProps = partition(entries, ([key]) => labelProps.test(key));
+  const splitProps = partition(entries, ([key]) => containerProps.test(key));
 
   return splitProps.map(Object.fromEntries) as [Label, Input];
 };
 
-const labelProps = /^aria-|^data-/;
+const containerProps = /^aria-|^data-/;
 
 type Input = JSX.IntrinsicElements['input'];
 type Label = JSX.IntrinsicElements['label'];
