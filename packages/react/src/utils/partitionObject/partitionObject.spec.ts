9d28e230d3534689143c463468507ab9706ed7b3
Andre Rabello <andre.rabello@onfido.com>
2021-01-28 13:22:08 +0000

    test: fix partitionObject

--- packages/react/src/utils/partitionObject/partitionObject.spec.ts
+++ packages/react/src/utils/partitionObject/partitionObject.spec.ts
@@ -5,7 +5,7 @@ describe('partitionObject', () => {
   it('should split an object like [{ "does" }, { "does not" }] satisfy the predicate', () => {
     const obj = { foo: true, bar: false };
 
-    const [truthy, falsy] = partitionObject(obj, Boolean);
+    const [truthy, falsy] = partitionObject(obj, ([, v]) => Boolean(v));
 
     expect(truthy).toStrictEqual({ foo: true });
     expect(falsy).toStrictEqual({ bar: false });
