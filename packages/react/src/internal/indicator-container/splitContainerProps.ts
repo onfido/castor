4379388dfe6979e667e7adc64193491a984c9b11
Andre Rabello <andre.rabello@onfido.com>
2021-01-28 15:21:10 +0000

    chore: prettier

--- packages/react/src/internal/indicator-container/splitContainerProps.ts
+++ packages/react/src/internal/indicator-container/splitContainerProps.ts
@@ -3,8 +3,9 @@ import { partitionObject } from '../../utils';
 /**
  * Splits `Indicator` properties into `label` and `input` props.
  */
-export const splitContainerProps = (props: InputElementProps | LabelElementProps) =>
-  partitionObject(props, ([key]) => containerProps.test(key));
+export const splitContainerProps = (
+  props: InputElementProps | LabelElementProps
+) => partitionObject(props, ([key]) => containerProps.test(key));
 
 const containerProps = /^aria-|^data-/;
 
