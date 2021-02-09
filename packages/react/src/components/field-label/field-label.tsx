9eeafef8f5b3b4f0ae95b3705d4a03536ebd7e5b
Julius Osokinas <julius.osokinas@onfido.com>
2021-02-09 13:59:31 +0000

    docs: inform that field-label should provide a text label for the field
    Co-Authored-By: Andre Luiz Rabello <andre.luiz_r@hotmail.com>

--- packages/react/src/components/field-label/field-label.tsx
+++ packages/react/src/components/field-label/field-label.tsx
@@ -2,10 +2,11 @@ import { c, classy } from '@onfido/castor';
 import React from 'react';
 
 /**
- * Intended to be used within `Input` and `Textarea` components.
+ * Intended to be used within `Input` and `Textarea` components, providing a
+ * text label for the field.
  *
- * Can also be used alongside these components, but should then be connected via
- * the "htmlFor" prop.
+ * Can also be used alongside these two components, but should then be connected
+ * via the "htmlFor" prop.
  */
 export const FieldLabel = ({
   className,
