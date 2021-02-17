91f0c9a91c7bdf5d19c3b6aee32205c49b605a15
Julius Osokinas <20243687+josokinas@users.noreply.github.com>
2021-02-17 13:08:08 +0000

    style: apply suggestions from code review
    Co-authored-by: Andre Luiz Rabello <andre.luiz_r@hotmail.com>

--- packages/react/src/components/asterisk/asterisk.tsx
+++ packages/react/src/components/asterisk/asterisk.tsx
@@ -2,14 +2,14 @@ import { c, classy } from '@onfido/castor';
 import React from 'react';
 
 /**
- * Intended to be used with `FieldLabel` component.
+ * Use in `FieldLabel` to denote required fields.
  */
 export const Asterisk = ({
   className,
   ...restProps
 }: AsteriskProps): JSX.Element => (
   <abbr {...restProps} className={classy(c('asterisk'), className)}>
-    &nbsp;{'*'}
+    {' *'}
   </abbr>
 );
 
