8f45c4eda8f32067f32ec8239ef4cf9c41e771a5
Julius Osokinas <julius.osokinas@onfido.com>
2021-03-05 12:13:53 +0000

    docs: add html input stories

---
+++ packages/core/src/components/input/input.story.tsx
@@ -0,0 +1,14 @@
+import { InputProps as BaseProps } from '@onfido/castor';
+import { html } from '../../../../../docs';
+import { c, classy, m } from '../../utils';
+
+export interface InputProps extends BaseProps {
+  value?: string;
+}
+
+export const Input = ({ type = 'text', invalid, ...props }: InputProps) =>
+  html('input', {
+    ...props,
+    class: classy(c('input'), m({ invalid })),
+    type,
+  });
