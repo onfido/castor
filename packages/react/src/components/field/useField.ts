1f3728182e8060de0ec5ae3c38d49898e3271d3f
Andre Luiz Rabello <andre.luiz_r@hotmail.com>
2021-04-20 20:44:09 +0100

    fix(react): useField types (#590)

--- packages/react/src/components/field/useField.ts
+++ packages/react/src/components/field/useField.ts
@@ -8,11 +8,13 @@ export const useField = () => {
   const form = useForm();
   const field = useContext(FieldContext);
 
-  return { ...omitNullish(form), ...omitNullish(field) };
+  return { ...omitNullish(form), ...omitNullish(field) } as FieldState;
 };
 
 export const FieldProvider = FieldContext.Provider;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const omitNullish = (obj: Record<string, any>) =>
-  Object.fromEntries(Object.entries(obj).filter(([, value]) => value != null));
+const omitNullish = <T extends Record<string, any>>(obj: T) =>
+  Object.fromEntries(
+    Object.entries(obj).filter(([, value]) => value != null)
+  ) as Partial<T>;
