f54a80e6bbc01e09f7f05d20059c103c419bfa5c
Julius Osokinas <20243687+josokinas@users.noreply.github.com>
2021-05-24 14:35:39 +0100

    docs: add aria-hidden="true" to icons inside buttons (#703)

--- packages/core/src/components/icon/icon.story.tsx
+++ packages/core/src/components/icon/icon.story.tsx
@@ -1,13 +1,18 @@
-import { c, classy, color, IconProps } from '@onfido/castor';
+import { c, classy, color, IconProps as BaseProps } from '@onfido/castor';
 import { html } from '../../../../../docs';
+
+export interface IconProps extends BaseProps {
+  'aria-hidden'?: 'true' | 'false';
+}
 
 /**
  * `.ods-icon` requires SVG sprite to be included in your app.
  *
  * https://github.com/onfido/castor-icons
  */
-export const Icon = ({ name, color: token }: IconProps) =>
+export const Icon = ({ name, color: token, ...props }: IconProps) =>
   html('svg', {
+    ...props,
     class: classy(c('icon')),
     fill: token ? color(token) : 'currentColor',
     focusable: 'false',
