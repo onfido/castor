58c46cef449388a821e546116e9d26d815574806
Andre Rabello <andre.rabello@onfido.com>
2021-07-27 19:04:44 +0100

    test: update wait conditions

--- e2e/visual-regression/all.e2e.ts
+++ e2e/visual-regression/all.e2e.ts
@@ -17,9 +17,17 @@ describe('components', () => {
 
       cy.visit(`/iframe.html?id=${id}`);
 
-      cy.get('body')
-        .should('have.class', 'castor-theme--day')
-        .matchImageSnapshot(name);
+      // use slightly higher timeout because sometimes the iframe is slow
+      cy.get('body', { timeout: 10000 }).should(
+        'have.class',
+        'castor-theme--day'
+      );
+
+      // give React one cycle to run effects and timeouts and stuff,
+      // and give the browser one requestFrame
+      cy.wait(20);
+
+      cy.get('body').matchImageSnapshot(name);
     });
   });
 });
