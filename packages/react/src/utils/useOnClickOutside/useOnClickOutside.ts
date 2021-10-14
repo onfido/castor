425b057146eda2592b08da7b426d8cccdea51d6b
Andre Rabello <andre.rabello@onfido.com>
2021-10-14 12:58:52 +0100

    style: variable rename

--- packages/react/src/utils/useOnClickOutside/useOnClickOutside.ts
+++ packages/react/src/utils/useOnClickOutside/useOnClickOutside.ts
@@ -6,7 +6,7 @@ import { RefObject, useEffect } from 'react';
  * @param targets RefObject of elements to consider "inside".
  */
 export const useOnClickOutside = <T extends Element>(
-  onClickOutside: ((ev: MouseEvent) => void) | undefined,
+  onClickOutside: ((event: MouseEvent) => void) | undefined,
   targets: RefObject<T>[]
 ) =>
   useEffect(() => {
@@ -15,9 +15,9 @@ export const useOnClickOutside = <T extends Element>(
     const elements = targets.map((t) => t.current).filter(Boolean) as T[];
     if (!elements.length) return;
 
-    const onClick = (ev: MouseEvent) =>
-      elements.some((element) => element.contains(ev.target as Element)) ||
-      onClickOutside(ev);
+    const onClick = (event: MouseEvent) =>
+      elements.some((element) => element.contains(event.target as Element)) ||
+      onClickOutside(event);
 
     document.addEventListener('click', onClick);
     return () => document.removeEventListener('click', onClick);
