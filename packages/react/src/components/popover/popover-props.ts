cf5d33d10aee96d2232f26812b820bf8a30193bb
Andre Rabello <andre.rabello@onfido.com>
2021-10-14 18:05:12 +0100

    docs: improve popover

--- packages/react/src/components/popover/popover-props.ts
+++ packages/react/src/components/popover/popover-props.ts
@@ -8,7 +8,17 @@ export interface PopoverProps extends BaseProps, Omit<Div, 'ref'> {
    * screen is too small.
    */
   overlay?: boolean;
+  /**
+   * An event that notifies the intention to close the Popover, usually from an
+   * outside click.
+   *
+   * Only triggers when target is specified.
+   */
   onClose?: () => void;
+  /**
+   * An event that notifies the initial rendering of the Popover has completed,
+   * provides the Popover element as argument.
+   */
   onRender?: (popover: HTMLDivElement | null) => void;
   /**
    * Ref to an element which the Popover should target for placement.
