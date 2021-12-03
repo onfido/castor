1b23a10fbd6ed5d9fbd74aae60880d6170ba0e46
Andre Luiz Rabello <andre.rabello@onfido.com>
2021-12-03 09:24:21 +0000

    refactor: storybook custom prepareForInline (#1201)

--- .storybook/custom/prepareForInline.tsx
+++ .storybook/custom/prepareForInline.tsx
@@ -7,21 +7,17 @@ export const prepareForInline = (
   storyFn: () => JSX.Element,
   ctx: StoryContext & ContainerContext
 ) => {
-  const story = storyFn();
-  const content: unknown = ctx.originalStoryFn(ctx.args as any, ctx);
-  const decorate = withDecorators(ctx);
+  const content = ctx.originalStoryFn(ctx.args as any, ctx);
 
-  if (typeof content === 'string')
-    return decorate(<HtmlContainer>{content}</HtmlContainer>);
-  else if (ctx.id.startsWith('css-')) return decorate(story);
+  if (typeof content == 'string')
+    return decorate(ctx, <HtmlContainer>{content}</HtmlContainer>);
 
-  return story;
+  return storyFn();
 };
 
 const HtmlContainer = ({ children }: { children: string }) => (
   <div ref={(node) => node && (node.outerHTML = children)} />
 );
 
-const withDecorators =
-  (ctx: StoryContext & ContainerContext) => (content: JSX.Element) =>
-    decorators.reduce((final, next) => next(() => final, ctx), content);
+const decorate = (ctx: StoryContext & ContainerContext, content: JSX.Element) =>
+  decorators.reduce((final, next) => next(() => final, ctx), content);
