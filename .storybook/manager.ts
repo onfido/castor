9271393161c8681c42596c7769f6c224e3765a7c
Andre Luiz Rabello <andre.rabello@onfido.com>
2022-01-06 12:04:08 +0000

    docs: replace "Storybook" in title with "Castor" (#1316)

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -9,6 +9,12 @@ export const mode = process.env.STORYBOOK_MODE; // defined on CI script
 if (env === 'production' && mode === 'production')
   window.STORYBOOK_GA_ID = process.env.STORYBOOK_GA_ID;
 
+// everytime Storybook changes the document title, replace it with "Castor"
+new MutationObserver(() => {
+  if (document.title.endsWith('Storybook'))
+    document.title = document.title.slice(0, -9) + 'Castor';
+}).observe(document.querySelector('title'), { childList: true });
+
 declare global {
   interface Window {
     STORYBOOK_GA_ID: string;
