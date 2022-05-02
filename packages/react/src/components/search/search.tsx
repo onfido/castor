c8cbd1d6298b4584278de63a49fa790be101465c
Andre Rabello <andre.rabello@onfido.com>
2022-05-02 15:15:08 +0100

    feat: allow external icon usage

--- packages/react/src/components/search/search.tsx
+++ packages/react/src/components/search/search.tsx
@@ -1,24 +1,30 @@
 import { c, classy } from '@onfido/castor';
-import { Icon, Input, InputProps } from '@onfido/castor-react';
+import { Input, InputProps } from '@onfido/castor-react';
 import React from 'react';
+import { MaybeIcon } from '../../internal';
 import { withRef } from '../../utils';
 
 /**
- * `Search` uses an `Icon` that requires `Icons` (SVG sprite) to be included in
- * your app.
+ * `Search` by default uses an `Icon` that requires `Icons` (SVG sprite) to be
+ * included in your app.
  *
  * https://github.com/onfido/castor-icons#use-with-plain-code
+ *
+ * You may also provide any other SVG element via the `icon` prop, but using
+ * Castor iconography is recommended.
  */
 export const Search = withRef(function Search(
-  { className, style, ...restProps }: SearchProps,
+  { className, icon, style, ...restProps }: SearchProps,
   ref: SearchProps['ref']
 ) {
   return (
     <div className={classy(c('search'), className)} style={style}>
       <Input {...restProps} ref={ref} type="search" />
-      <Icon name="search" aria-hidden="true" />
+      <MaybeIcon icon={icon} name="search" />
     </div>
   );
 });
 
-export type SearchProps = Omit<InputProps, 'type' | 'invalid' | 'children'>;
+export type SearchProps = Omit<InputProps, 'type' | 'invalid' | 'children'> & {
+  icon?: JSX.Element;
+};
